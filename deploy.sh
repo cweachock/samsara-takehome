ed4d5945968de118527747cbdc5cf955c9dc9c2d
Chris Weachock <cweachock@gmail.com>
2022-02-04 13:47:55 -0800

    github deploy sh

--- deploy.sh
+++ deploy.sh
@@ -14,6 +14,6 @@ git init
 git add -A
 git commit -m 'deploy'
 
-git push -f git@github.com:cweachock/vue-practice.git master:gh-pages
+git push -f git@github.com:cweachock/samsara-takehome.git master:gh-pages
 
 cd -
