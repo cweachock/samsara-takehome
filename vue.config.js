ce75ca2c31d7562d7736d950c2408caf756a42e2
Chris Weachock <cweachock@gmail.com>
2022-02-04 15:29:08 -0800

    update publicPath in config

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,5 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === "production" ? "/REPO_NAME/" : "/",
+  publicPath:
+    process.env.NODE_ENV === "production" ? "/samsara-takehome/" : "/",
   outputDir: "docs",
 };
